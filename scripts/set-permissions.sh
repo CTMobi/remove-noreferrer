246acbe126f5814850665525068f6ae2753d4050
Alessandro Carrera <alessandro.carrera@ctmobi.it>
2021-04-30 17:38:24 +0200

    Revert modifiche non necessarie

--- scripts/set-permissions.sh
+++ scripts/set-permissions.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
 chown -R zerounoweb:zerounoweb /home/zerounoweb/zerounoweb.it/public_html/wp-content/plugins/remove-noreferrer
-chown -R energyup:energyup /home/energyup/energyup.tech/public_html/wp-content/plugins/remove-noreferrer
