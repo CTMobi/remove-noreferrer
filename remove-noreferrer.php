31941bedd72a9b5bb3012b4b79be07eb0ca9a22c
Matteo Pidone <matteo.pidone@ctmobi.it>
2023-03-21 17:00:37 +0100

    docs(readme): update readme

--- remove-noreferrer.php
+++ remove-noreferrer.php
@@ -11,7 +11,7 @@
  * Plugin Name:       Remove Noreferrer
  * Plugin URI:        https://wordpress.org/plugins/remove-noreferrer/
  * Github URI:        https://github.com/gruz0/remove-noreferrer
- * Description:       Removes rel="noreferrer" attribute from links on your website on-the-fly
+ * Description:       Removes rel="noreferrer" attribute from links on your website on-the-fly. Fork di CTMobi, aggiunta opzione per rimuovere attributo target="_blank".
  * Version:           5.0.0
  * Requires at least: 5.1
  * Requires PHP:      5.6
